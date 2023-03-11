26ca4831dc668e1eba291738672cbe7d635dbb09
史磊 <slshsl@qq.com>
2023-03-11 22:46:57 +0800

    feat: 2023-03-11-Webpack plugin（简单小例子）

---
+++ 2023/2023-03-11-Webpack plugin（简单小例子）/demos/demo1/webpack.config.js
@@ -0,0 +1,16 @@
+const Plugin1 = require("./plugins/Plugin1");
+const Plugin2 = require("./plugins/Plugin2");
+
+const CopyWebpackPlugin = require("./plugins/CopyWebpackPlugin");
+
+module.exports = {
+  plugins: [
+    // new Plugin1(),
+    // new Plugin2(),
+    new CopyWebpackPlugin({
+      from: "public",
+      // to: ".",
+      ignore: ["**/index.html"],
+    }),
+  ],
+};
